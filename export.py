672a487b0b598381ec2f28f30858881a6d5c6cc7
Alex Shand <alex@alexshand.com>
2019-01-20 21:56:26 +0000

    Moved export.pyi into export.py

--- export.py
+++ export.py
@@ -2,8 +2,14 @@
 
 import sys
 
+from typing import TypeVar, Type
+
+from type_aliases import Function
+
+C = TypeVar('C') # pylint: disable=invalid-name
+
 # From: https://speakerdeck.com/dabeaz/modules-and-packages-live-and-let-die?slide=56
-def export(func):
+def export(func: Function) -> Function:
     """Module level export decorator, appends function name to __all__."""
     mod = sys.modules[func.__module__]
     all_list = getattr(mod, '__all__', [])
@@ -13,18 +19,18 @@ def export(func):
 export(export)
 
 @export
-def cls_export(func):
+def cls_export(func: Function) -> Function:
     """Class level export decorator, should be used in conjuction with
     collect_exports."""
     setattr(func, '_exported', True)
     return func
 
 @export
-def collect_exports(cls):
+def collect_exports(cls: Type[C]) -> Type[C]:
     """Fill the __all__ list of a class with functions marked with _exported."""
     for name, func in cls.__dict__.items():
         if getattr(func, '_exported', False):
             all_list = getattr(cls, '__all__', [])
-            all_list.append(func.__name__)
+            all_list.append(name)
             setattr(cls, '__all__', all_list)
     return cls
